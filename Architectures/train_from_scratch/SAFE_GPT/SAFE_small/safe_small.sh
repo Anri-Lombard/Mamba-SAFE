ea2c170b6eb80296545a315b1d7bec64a8add4e3
Anri-Lombard <anri.m.lombard@gmail.com>
2024-06-14 20:21:13 +0200

    Correct model code

--- Architectures/train_from_scratch/SAFE_GPT/SAFE_small/safe_small.sh
+++ Architectures/train_from_scratch/SAFE_GPT/SAFE_small/safe_small.sh
@@ -1,16 +1,57 @@
 #!/bin/bash
 #SBATCH --account=nlpgroup
 #SBATCH --partition=a100
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
 #SBATCH --gres=gpu:ampere:1
-#SBATCH --time=04:00:00
+#SBATCH --time=48:00:00
 #SBATCH --job-name="SAFE_small"
 #SBATCH --mail-user=lmbanr001@myuct.ac.za
 #SBATCH --mail-type=ALL
 
 # Set CUDA_VISIBLE_DEVICES
-CUDA_VISIBLE_DEVICES=$(ncvd)
+export CUDA_VISIBLE_DEVICES=$(ncvd)
 
-export WANDB_DISABLED="true"
+# export WANDB_MODE="disabled"
+export WANDB_MODE="offline"
+export WANDB_API_KEY="d68cdf1a94da49b43fbfb7fd90246c39d7c34237"
+# Set the wandb cache and directory paths
+wandb_cache_dir="/scratch/lmbanr001/wandb_cache"
+wandb_dir="/scratch/lmbanr001/wandb"
+
+# Check if the wandb cache directory exists
+if [ -d "$wandb_cache_dir" ]; then
+  # If it exists, remove all its contents
+  rm -rf "$wandb_cache_dir"/*
+else
+  # If it doesn't exist, create the directory
+  mkdir -p "$wandb_cache_dir"
+fi
+
+# Check if the wandb directory exists
+if [ -d "$wandb_dir" ]; then
+  # If it exists, remove all its contents
+  rm -rf "$wandb_dir"/*
+else
+  # If it doesn't exist, create the directory
+  mkdir -p "$wandb_dir"
+fi
+
+# Set the wandb environment variables
+export WANDB_CACHE_DIR=$wandb_cache_dir
+export WANDB_DIR=$wandb_dir
+
+# Set the TMPDIR environment variable
+export TMPDIR="/scratch/lmbanr001/tmp"
+
+# Check if the TMPDIR directory exists
+if [ -d "$TMPDIR" ]; then
+  # If it exists, remove all its contents
+  rm -rf "$TMPDIR"/*
+else
+  # If it doesn't exist, create the directory
+  mkdir -p "$TMPDIR"
+fi
 
 # Load necessary modules
 module load python/miniconda3-py310
@@ -21,25 +62,28 @@ source activate architecture_venv
 config_path="../trainer/configs/small_config.json"
 tokenizer_path="../tokenizer.json"
 dataset_path="../../Datasets/MOSES/datasets"
-output_dir="./trained/SAFE_small"
+output_dir="/scratch/lmbanr001/SAFE_small"
+
+mkdir -p $output_dir
+mkdir -p $wandb_cache_dir
 
 safe-train --config $config_path \
   --tokenizer $tokenizer_path \
   --dataset $dataset_path \
-  --report_to none \
   --text_column "SAFE" \
   --torch_compile True \
   --optim "adamw_torch" \
-  --learning_rate 1e-5 \
+  --num_train_epochs 1000 \
+  --learning_rate 5e-4 \
+  --per_device_train_batch_size 16 \
+  --gradient_accumulation_steps 8 \
+  --eval_steps 500 \
+  --save_steps 500 \
+  --save_total_limit 2 \
   --prop_loss_coeff 1e-3 \
-  --gradient_accumulation_steps 1 \
   --output_dir $output_dir \
   --overwrite_output_dir True \
-  --max_steps 5 \
-  --do_train True \
-  --disable_tqdm True
-
-# Need to add do_train and do_eval
+  --do_train True
 
 # Deactivate virtual environment
-conda deactivate(architecture_venv)
+conda deactivate
